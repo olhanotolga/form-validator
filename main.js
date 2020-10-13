1c9516b281d3d0f86e3e463998d4b2440bd8bddc
Olha Halat <olha.halat@gmail.com>
2020-10-13 13:13:01 +0200

    separate the html, css, and js files

--- main.js
+++ main.js
@@ -47,3 +47,60 @@ password.onkeyup = function validatePassword() {
         minCharsElement.classList.remove("valid");
     }
 };
+
+const email = document.getElementById("email");
+
+email.onkeyup = function validateEmail() {
+    console.log("character typed: ", email.value);
+
+    // patterns
+    // const commAtChar = String.fromCharCode(64);
+    const commAtChar = "@";
+    console.log(commAtChar, typeof commAtChar);
+    const dotChar = ".";
+
+    const atCharElement = document.getElementById("at-character");
+    const dotCharElement = document.getElementById("dot-character");
+    const minCharsEmailElement = document.getElementById("min-email-chars");
+
+    if (email.value.includes(commAtChar)) {
+        console.log(
+            password.value,
+            commAtChar,
+            "includes @:",
+            password.value.includes(commAtChar)
+        );
+
+        atCharElement.classList.remove("invalid");
+        atCharElement.classList.add("valid");
+    } else {
+        console.log(
+            password.value,
+            "includes @:",
+            password.value.includes(commAtChar)
+        );
+
+        atCharElement.classList.add("invalid");
+        atCharElement.classList.remove("valid");
+    }
+
+    // at least 2 characters before @
+
+    if (email.value.includes(dotChar)) {
+        dotCharElement.classList.remove("invalid");
+        dotCharElement.classList.add("valid");
+    } else {
+        dotCharElement.classList.add("invalid");
+        dotCharElement.classList.remove("valid");
+    }
+
+    // . is after the @ character
+
+    if (email.value.length >= 6) {
+        minCharsEmailElement.classList.remove("invalid");
+        minCharsEmailElement.classList.add("valid");
+    } else {
+        minCharsEmailElement.classList.add("invalid");
+        minCharsEmailElement.classList.remove("valid");
+    }
+};
